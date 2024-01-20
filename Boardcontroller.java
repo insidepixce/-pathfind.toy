efc53939740182faa5455f8e016904f84b98afc3
cree-per-com <kcossld-naver.com>
2024-01-20 14:54:12 +0900

    Revert "검색 기능 추가 By JpaRepository)"
    This reverts commit 197a1ca97185a04094cd9022cc2c763819d54c0f.

--- Boardcontroller.java
+++ Boardcontroller.java
@@ -3,18 +3,13 @@ package com.study.board.controller;
 import com.study.board.entity.Board;
 import com.study.board.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 
@@ -23,42 +18,32 @@ public class Boardcontroller {
     private BoardService boardService;
 
     @GetMapping("/board/write")//localhost:8080/board/write
-    public String boardWriteForm() {
-
+    public String boardWriteFrom() {
         return "boardwrite";
-
     }
 
     @PostMapping("/board/writepro")
-    public String boardWritePro(Board board, Model model, MultipartFile file) throws Exception {
+    public String boardWritePro(Board board, Model model) {
 
-        boardService.write(board, file);
+        boardService.write(board);
 
-        model.addAttribute("message", "글 작성이 완료되었습니다.");
-        model.addAttribute("searchUrl", "/board/list");
+        model.addAttribute("message", "글 작성이 완료되었습니다."); //작성완료버튼을 눌렀을 때 띄워줄 메세지 내용입니다.
+        model.addAttribute("searchUrl", "/board/list"); //model에 담겨서 message.html에 넘어옵니다(??)
 
         return "message";
-
     }
 
     //게시글 리스트 처리
     @GetMapping("/board/list")
-    public String boardList(Model model,
-                            @PageableDefault(page=0, size=10, sort="id", direction = Sort.Direction.DESC)
-                            Pageable pageable, String searchKeyword)
+    public String boardList(Model model, @PageableDefault(page=0, size=10, sort="id",
+            direction = Sort.Direction.DESC) Pageable pageable)
     //디폴트 페이지:0, 한 페이지당 게시글 수:10개, 정렬 기준 방식: id, 정렬 순서:역순
     {
-        Page<Board> list = null;
+        Page<Board> list = boardService.boardList(pageable);
 
-        if(searchKeyword == null) {
-            list = boardService.boardList(pageable);} //검색 단어 미입력: 기존 리스트 그대로 출력
-        else {
-            list = boardService.boardSearchList(searchKeyword, pageable); //검색 단어 입력 : 검색 기능이 포함된 리스트를 출력
-        }
-
-        int nowPage = list.getPageable().getPageNumber() + 1; //Pageable에서 넘어온 현재 페이지. 시작값이 0이라 1을 더함.
-        int startPage = Math.max(nowPage - 4, 1); //블럭에서 보여줄 시작 페이지. Math.max로 페이지번호가 음수가 되는 오류 수정.
-        int endPage = Math.min(nowPage + 5, list.getTotalPages()); //블럭에서 보여줄 마지막 페이지. Math.min으로 페이지번호가 전체 페이지수보다 클 경우 totalpages를 반환.
+        int nowPage = list.getPageable().getPageNumber() + 1; //Pageable에서 넘어온 현재 페이지. 시작값이 0이기 때문에 편의를 위하여 1을 더해주었습니다.
+        int startPage = Math.max(nowPage - 4, 1); //블럭에서 보여줄 시작 페이지. Math.max를 이용해 페이지 번호가 음수가 되지 않도록 설정했습니다.
+        int endPage = Math.min(nowPage + 5, list.getTotalPages()); //블럭에서 보여줄 마지막 페이지. Math.min을 이용해 페이지 번호가 총 페이지수를 넘을경우 totalpages를 반환하도록 설정했습니다.
 
         model.addAttribute("list", boardService.boardList());
         model.addAttribute("nowPage", nowPage);
@@ -66,29 +51,6 @@ public class Boardcontroller {
         model.addAttribute("endPage", endPage);
 
         return "boardlist";
-    }
-
-    @PostMapping("/board/update/{id}")
-    public String boardUpdate(@PathVariable("id") Integer id, Board board, MultipartFile file) throws Exception {
-
-        Board boardTemp = boardService.boardView(id);
-        boardTemp.setTitle(board.getTitle());
-        boardTemp.setContent(board.getContent());
-
-        boardService.write(boardTemp, file);
-
-        return "redirect:/board/list";
-
-    }
-
-
-    @GetMapping("/board/modify/{id}")
-    public String boardModify(@PathVariable("id") Integer id,
-                              Model model) {
-
-        model.addAttribute("board",boardService.boardView(id));
-
-        return "boardmodify";
     }
 
     @GetMapping("/board/view") // localhost:8080/board/view?id=1
